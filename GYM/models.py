3e5d31ce51edea4fa7ddd241de1101e406e5b527
yash9650 <yash1602200@gmail.com>
2021-04-12 23:40:36 +0530

    Initial commit

---
+++ GYM/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Enquiry(models.Model):
+    name = models.CharField(max_length=60)
+    contact = models.CharField(max_length=10)
+    emailid = models.CharField(max_length=60)
+    age = models.CharField(max_length=10)
+    gender = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.CharField(max_length=10)
+    unit = models.CharField(max_length=10)
+    date = models.CharField(max_length=40)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Plan(models.Model):
+    name = models.CharField(max_length=30)
+    amount = models.CharField(max_length=20)
+    duraton = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Member(models.Model):
+    name = models.CharField(max_length=60)
+    contact = models.CharField(max_length=10)
+    emailid = models.CharField(max_length=60)
+    age = models.CharField(max_length=10)
+    gender = models.CharField(max_length=10)
+    plan = models.CharField(max_length=10)
+    joindate = models.CharField(max_length=20)
+    expiredate = models.CharField(max_length=20)
+    initalamount = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
